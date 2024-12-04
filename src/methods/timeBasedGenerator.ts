b6c45fe5f775d00545b25261dfe8418836f1848a
It's Satyajit💫 <77627641+Its-Satyajit@users.noreply.github.com>
2024-12-04 20:40:17 +0530

    feat: refactor-based ID generation to functions
    Replace the `TimeBasedGenerator` class with standalone functions 
    `generateTimeBasedId` and `validateTimeBasedId`. This change 
    simplifies the code structure by removing unnecessary class 
    overhead and directly utilizing the `generateRandomId` function 
    for random ID generation.

--- src/methods/timeBasedGenerator.ts
+++ src/methods/timeBasedGenerator.ts
@@ -1,28 +1,12 @@
-import { BaseIdGenerator } from "./baseGenerator";
-import { RandomIdGenerator } from "./randomGenerator";
+import { generateRandomId } from "./randomIDGenerator";
 
-/**
- * A concrete ID generator that produces sortable time-based IDs.
- * This class extends the `BaseIdGenerator` and generates IDs in the format `base36(timestamp)-base62(random)`.
- */
+export function generateTimeBasedId(): string {
+  const timestamp = Date.now().toString(36);
+  const random = generateRandomId(8);
+  return `${timestamp}-${random}`;
+}
 
-export class TimeBasedGenerator extends BaseIdGenerator {
-  /**
-   * Generates a sortable time-based ID.
-   * The format of the ID is `base36(timestamp)-base62(random)`.
-   * @returns A time-based ID string.
-   * @example
-   * const timeBasedId = new TimeBasedGenerator().generate();
-   */
-
-  generate(): string {
-    const timestamp = Date.now().toString(36);
-    const random = new RandomIdGenerator().generate(8);
-    return `${timestamp}-${random}`;
-  }
-
-  validate(id: string): boolean {
-    console.log(id);
-    return true;
-  }
+export function validateTimeBasedId(id: string): boolean {
+  console.log(id);
+  return true;
 }
